0b80cd10b3e0b2913de814cd92fc0666f7456f1c
Marvin Wright <github@wrig.de>
2022-03-22 06:55:39 +0100

    fix in/out issue

--- naive_implementation.R
+++ naive_implementation.R
@@ -20,7 +20,7 @@ y <- lp + rnorm(n)
 dat <- data.frame(y = y, x)
 
 # xgboost
-xg <- xgboost(data = x, label = y, params = list(max_depth = 3, eta = .5), nrounds = 1)
+xg <- xgboost(data = x, label = y, params = list(max_depth = 5, eta = .5), nrounds = 3)
 trees <- xgb.model.dt.tree(model = xg, use_int_id = TRUE)
 
 # Function to get all subsets of set
@@ -30,7 +30,7 @@ subsets <- function(x) {
 
 # Recursively go through trees
 # This calculates the expectation of a tree when the given features are in the coalition
-recurse <- function(tree, node_id, feats_in, obs) {
+recurse <- function(tree, node_id, feats_out, obs) {
   #message("node id ", node_id)
   if (trees[Tree == tree & Node == node_id, Feature == "Leaf"]) {
     # For leaf, return prediction
@@ -44,9 +44,9 @@ recurse <- function(tree, node_id, feats_in, obs) {
       next_id <-  trees[Tree == tree & Node == node_id, No]
     }
 
-    if (feat %in% feats_in) {
+    if (!(feat %in% feats_out)) {
       # If splitting variable is in, split
-      recurse(tree, next_id, feats_in, obs)
+      recurse(tree, next_id, feats_out, obs)
     } else {
       # If not, weighted average
       yes <- trees[Tree == tree & Node == node_id, Yes]
@@ -55,25 +55,23 @@ recurse <- function(tree, node_id, feats_in, obs) {
       cover_yes <- trees[Tree == tree & Node == yes, Cover]
       cover_no <- trees[Tree == tree & Node == no, Cover]
 
-      exp_yes <- recurse(tree, yes, feats_in, obs)
-      exp_no <- recurse(tree, no, feats_in, obs)
+      exp_yes <- recurse(tree, yes, feats_out, obs)
+      exp_no <- recurse(tree, no, feats_out, obs)
 
       cover_yes / cover_own * exp_yes + cover_no / cover_own * exp_no
     }
   }
 }
 
-# Function to calculate m_S (model with feature subset S)
+# Function to calculate m_S (model without feature subset S)
 m_S <- function(obs, S) {
   sum(sapply(0:max(trees$Tree), function(tree) {
     if (trees[Tree == tree, all(S %in% Feature)]) {
       # For all subsets U in tree
       T <- trees[Tree == tree & Feature != "Leaf", unique(Feature)]
       sum(sapply(subsets(T), function(U) {
-        if ((length(setdiff(T, S)) == 0) || (setdiff(T, S) %in% U)) {
-          # TODO: Question: Why do we need that additional (-1)*?
-          #(-1)^(length(S) - length(setdiff(T, U))) * recurse(tree = tree, node_id = 0, feats_in = U, obs = obs)
-          (-1) * (-1)^(length(S) - length(setdiff(T, U))) * recurse(tree = tree, node_id = 0, feats_in = U, obs = obs)
+        if ((length(setdiff(T, S)) == 0) || all(setdiff(T, S) %in% U)) {
+          (-1)^(length(S) - length(setdiff(T, U))) * recurse(tree = tree, node_id = 0, feats_out = U, obs = obs)
         } else {
           0
         }
@@ -108,9 +106,5 @@ m_all[-1] / d[-1]
 # Interaction shap
 treeshap(uxg, x[1, , drop = FALSE], interactions = TRUE)
 
-# TODO: Sometimes wrong for depth >= 3, why?
-# TODO: Question: No unique solution?
 # TODO: Question: Our advantage? Higher-order interactions, faster? Or same speed?
 # TODO: Fast implementation
-
-
