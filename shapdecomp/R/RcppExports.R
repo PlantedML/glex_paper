f67f9db2b9c55edd624c30513465478e89ac511b
Marvin Wright <github@wrig.de>
2022-04-29 15:14:38 +0200

    fix Rcpp issues in package

--- shapdecomp/R/RcppExports.R
+++ shapdecomp/R/RcppExports.R
@@ -2,10 +2,10 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 recurse <- function(x, feature, split, yes, no, quality, cover, U, node) {
-    .Call('_shapdecomp_recurse', PACKAGE = 'shapdecomp', x, feature, split, yes, no, quality, cover, U, node)
+    .Call(`_shapdecomp_recurse`, x, feature, split, yes, no, quality, cover, U, node)
 }
 
 contribute <- function(mat, m_all, S, T, T_subsets, colnum) {
-    invisible(.Call('_shapdecomp_contribute', PACKAGE = 'shapdecomp', mat, m_all, S, T, T_subsets, colnum))
+    invisible(.Call(`_shapdecomp_contribute`, mat, m_all, S, T, T_subsets, colnum))
 }
 
--- shapdecomp/R/shapdecomp.R
+++ shapdecomp/R/shapdecomp.R
@@ -6,6 +6,7 @@
 #'
 #' @return decomposition
 #' @useDynLib shapdecomp, .registration = TRUE
+#' @import Rcpp
 #' @import data.table
 #' @export
 #'
